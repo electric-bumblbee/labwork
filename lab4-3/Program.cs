81d0832b54b8327c0f23b0f019c8bfbaf8e0aa4f
Anastasia Kochkova <93382533+electric-bumblbee@users.noreply.github.com>
2021-11-27 15:04:41 +0300

    Add files via upload

---
+++ lab4-3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace lab4_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Лабораторная работа №4 Задание 3 (＾• ω •＾)";
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+            Console.WriteLine("Введите число а");
+            int a = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Введите число b");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int S = (a + b)/2;
+
+            Console.WriteLine($"Среднее арифметическое a и b равно {S}");
+
+            Console.ReadKey();
+        }
+    }
+}
