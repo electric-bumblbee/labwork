256bc7051640ad27ae8e06aaeb52d1995ab73138
Anastasia Kochkova <93382533+electric-bumblbee@users.noreply.github.com>
2021-11-27 15:02:08 +0300

    Add files via upload

---
+++ lab4-1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace lab4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Лабораторная работа №4 Задание 1(＾• ω •＾)";
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("Введите значение стороны а");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите значение стороны b");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int P = 2 * (a + b);
+            int S = a * b;
+
+            Console.WriteLine($"Площадь прамоугольника равна {S}");
+            Console.WriteLine($"Периметр прямоугольника равен {P}");
+
+            Console.ReadKey();
+        }
+    }
+}
