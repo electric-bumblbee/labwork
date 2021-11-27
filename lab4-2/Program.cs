1422ee2ed8693ebabd43fd4b03fd1c79b5c3e5f3
Anastasia Kochkova <93382533+electric-bumblbee@users.noreply.github.com>
2021-11-27 15:03:22 +0300

    Add files via upload

---
+++ lab4-2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace lab4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Лабораторная работа №4 Задание 2 (＾• ω •＾)";
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+
+            Console.WriteLine("Введите диамер окружности ");
+            int d = Convert.ToInt32(Console.ReadLine());
+
+            double L = 3.14 * d;
+
+            Console.WriteLine($"Длина окружности равна {L}");
+
+            Console.ReadKey();
+        }
+    }
+}
